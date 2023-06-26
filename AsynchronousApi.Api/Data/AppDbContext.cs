e97a4c20bae795644115b75c05c2bed49fa2a8d7
Aram Zhamkochyan <aramzham@hotmail.com>
2023-06-27 00:29:54 +0200

    start endpoint

---
+++ AsynchronousApi.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AsynchronousApi.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsynchronousApi.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<ListingRequest> ListingRequests => Set<ListingRequest>();
+}
--- AsynchronousApi.Api/Program.cs
+++ AsynchronousApi.Api/Program.cs
@@ -1,6 +1,32 @@
+using AsynchronousApi.Api;
+using AsynchronousApi.Api.Data;
+using AsynchronousApi.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContextPool<AppDbContext>(o => o.UseSqlite("Data Source=RequestDb.db"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// start endpoint
+app.MapPost("/api/products", async (AppDbContext db, ListingRequest? listingRequest) =>
+{
+    if (listingRequest is null)
+        return Results.BadRequest();
+
+    var response = listingRequest with
+    {
+        RequestStatus = RequestStatus.Accepted,
+        EstimatedCompletionTime = DateTime.Now.AddHours(1)
+    };
+    
+    await db.AddAsync(response);
+    await db.SaveChangesAsync();
+    
+    return Results.Accepted($"api/productstatus/{response.RequestId}", response);
+});
+
+app.UseHttpsRedirection();
 
 app.Run();
